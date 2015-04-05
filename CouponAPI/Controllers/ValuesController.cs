4f4be727a098e1925011ddb15de21c2059f1d73b
happy-demon <fei@sgts.us>
2015-04-04 21:59:06 -0700

    Table Storage
    Added codes to read data from table storage.

--- CouponAPI/Controllers/ValuesController.cs
+++ CouponAPI/Controllers/ValuesController.cs
@@ -4,41 +4,112 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
+using System.Configuration;
 
 namespace CouponAPI.Controllers
 {
     // [Authorize]
-    public class Store
+
+    public class Coupon : TableEntity
     {
-        public int ID { get; set; }
-        public string Name { get; set; }
+        public string Category { get; set; }
+        public string CouponDetail { get; set; }
+        public DateTime CouponEndDate { get; set; }
+        public string CouponImage { get; set; }
+        public DateTime CouponStartDate { get; set; }
+        public string OriginalPrice { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductName { get; set; }
+        public string SaleCity { get; set; }
+        public string SalePrice { get; set; }
     }
 
-    public class StoreController : ApiController
+    public class OutletsController : ApiController
     {
+
         // GET api/values
-        public IEnumerable<Store> Get()
+        public IEnumerable<Coupon> Get()
         {
-            return new List<Store> {
-                new Store{ ID = 0, Name = "Costco" },
-                new Store{ ID = 1, Name = "Macy" },
-                new Store{ ID = 2, Name = "Outlets" }
-            };
+            // Retrieve the storage account from the connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                ConfigurationManager.AppSettings["StorageConnectionString"]);
+
+            // Create the table client.
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            // Create the CloudTable object that represents the "people" table.
+            CloudTable table = tableClient.GetTableReference("Coupons");
+
+            var results = (from Coupons in table.CreateQuery<Coupon>()
+                           where Coupons.PartitionKey == "奥特莱"
+                           select Coupons).Take(10).ToList();
+
+            return new List<Coupon>(results);
+
         }
 
         // GET api/values/5
-        public Store Get(int id)
+        public Coupon Get(int id)
         {
             return null;
         }
 
         // POST api/values
-        public void Post([FromBody]Store value)
+        public void Post([FromBody]Coupon value)
         {
         }
 
         // PUT api/values/5
-        public void Put(int id, [FromBody]Store value)
+        public void Put(int id, [FromBody]Coupon value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public class CostcoController : ApiController
+    {
+
+        // GET api/values
+        public IEnumerable<Coupon> Get()
+        {
+            // Retrieve the storage account from the connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                ConfigurationManager.AppSettings["StorageConnectionString"]);
+
+            // Create the table client.
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            // Create the CloudTable object that represents the "people" table.
+            CloudTable table = tableClient.GetTableReference("Coupons");
+
+            var results = (from Coupons in table.CreateQuery<Coupon>()
+                           where Coupons.PartitionKey == "好市多"
+                           select Coupons).Take(10).ToList();
+
+            return new List<Coupon>(results);
+
+        }
+
+        // GET api/values/5
+        public Coupon Get(int id)
+        {
+            return null;
+        }
+
+        // POST api/values
+        public void Post([FromBody]Coupon value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]Coupon value)
         {
         }
 
