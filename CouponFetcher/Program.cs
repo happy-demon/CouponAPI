2c21e1bbaf430f5525e8617307a62bb962ae91cf
tracyqi <qixi@msn.com>
2015-04-07 20:35:49 -0700

    webjobs fix

---
+++ CouponFetcher/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.WindowsAzure.Storage.Table;
+using System.IO;
+
+namespace CouponFetcher
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see http://go.microsoft.com/fwlink/?LinkID=320976
+    class Program
+    {
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+        static void Main()
+        {
+            JobHost host = new JobHost();
+            Task callTask = host.CallAsync(typeof(Program).GetMethod("ManualTrigger"));
+
+            Console.WriteLine("Waiting for async operation...");
+            callTask.Wait();
+            Console.WriteLine("Task completed: " + callTask.Status);
+
+        }
+
+
+        [NoAutomaticTrigger]
+        public static void ManualTrigger([Table("Coupons")] CloudTable couponsTable)
+        {
+            DateTime dt = DateTime.Now;
+
+            Coupon c = new Coupon()
+            {
+                PartitionKey = dt.Year.ToString(),
+                RowKey = dt.Month.ToString(),
+                Category = "test",
+                CouponDetail = "CouponDetail", CouponEndDate= DateTime.Now, CouponImage ="http://test", CouponStartDate = DateTime.Now , ETag ="", OriginalPrice = "10.00", ProductDescription="ffff", ProductName="ffff", SaleCity="seattle", SalePrice="8.20"
+            };
+
+            TableOperation operation = TableOperation.InsertOrReplace(c);
+
+            couponsTable.Execute(operation);
+        }
+    }
+
+    public class Coupon : TableEntity
+    {
+        public string Category { get; set; }
+        public string CouponDetail { get; set; }
+        public DateTime CouponEndDate { get; set; }
+        public string CouponImage { get; set; }
+        public DateTime CouponStartDate { get; set; }
+        public string OriginalPrice { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductName { get; set; }
+        public string SaleCity { get; set; }
+        public string SalePrice { get; set; }
+    }
+
+}
