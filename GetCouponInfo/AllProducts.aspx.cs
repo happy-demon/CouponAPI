95e58580a7defd773c40f640706f6b3987991cf9
happy-demon <demon.cheng@outlook.com>
2015-04-19 20:05:47 -0700

    Fixed

--- GetCouponInfo/AllProducts.aspx.cs
+++ GetCouponInfo/AllProducts.aspx.cs
@@ -11,11 +11,22 @@ public partial class AllProducts : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        var url = "http://coderwall.com/mdeiters.json";
+        var url = "http://couponapi.azurewebsites.net/api/coupons";
         var allProducts = _download_serialized_json_data<Product>(url);
-        Response.Write(allProducts.ProductId);
+        foreach (var x in allProducts)
+        {
+            Response.Write("<img src=" + x.ProductPicture + ">" + "<BR/>");
+            Response.Write("产品名称： " + x.ProductName + "<BR/>");
+            Response.Write("产品类别： " + x.ProductCategory + "<BR/>");
+            Response.Write("产品链接： " + x.ProductLink + "<BR/>");
+            Response.Write("起始日期： " + x.StartDate + "<BR/>");
+            Response.Write("结束日期： " + x.EndDate + "<BR/>");
+            Response.Write("折扣价格： " + x.OfferPrice + "<BR/>");
+            Response.Write("<BR/>");
+        }
+
     }
-    private static T _download_serialized_json_data<T>(string url) where T : new()
+    private static List<T> _download_serialized_json_data<T>(string url)
     {
         using (var w = new WebClient())
         {
@@ -27,15 +38,24 @@ public partial class AllProducts : System.Web.UI.Page
             }
             catch (Exception) { }
             // if string with JSON data is not empty, deserialize it to class and return its instance 
-            return !string.IsNullOrEmpty(json_data) ? JsonConvert.DeserializeObject<T>(json_data) : new T();
+            return JsonConvert.DeserializeObject<List<T>>(json_data);
         }
     }
     public class Product
     {
-        public string StoreName { get; set; }
-        public string StartDate { get; set; }
-        public string EndDate { get; set; }
-        public string Event { get; set; }
+        public string ProductCategory { get; set; }
+        public string ProductName { get; set; }
+        public object ItemNumber { get; set; }
+        public string ProductLink { get; set; }
+        public string ProductPicture { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductDescription_cn { get; set; }
+        public string OriginalPrice { get; set; }
+        public object LessPrice { get; set; }
+        public string OfferPrice { get; set; }
+        public object StartDate { get; set; }
+        public object EndDate { get; set; }
+        public object ProductReview { get; set; }
         public string PartitionKey { get; set; }
         public string RowKey { get; set; }
         public DateTime Timestamp { get; set; }
--- GetCouponInfo/App_Code/Product.cs
+++ GetCouponInfo/App_Code/Product.cs
@@ -6,18 +6,30 @@ using System.Web;
 /// <summary>
 /// Summary description for Product
 /// </summary>
-{"StoreName":"Wilsons Leather","StartDate":"4/16/2015","EndDate":"4/19/2015","Event":"Outerwear & Handbag Liquidation\nTake an Extra 70% Off Select Styles Plus an Additional 20% Off Select Styles\n\n*Some exclusions apply, offer subject to change. See Store for Details.\n","PartitionKey":"Outlets","RowKey":"01e92cfe-40c1-40b4-a9b7-dfb2ded202f7","Timestamp":"2015-04-19T22:37:25.0213132+00:00","ETag":"W/\"datetime'2015-04-19T22%3A37%3A25.0213132Z'\""}
 public class Rootobject
 {
-public string StoreName { get; set; }
-public string StartDate { get; set; }
-public string EndDate { get; set; }
-public string Event { get; set; }
+public string ProductCategory { get; set; }
+public string ProductName { get; set; }
+public object ItemNumber { get; set; }
+public string ProductLink { get; set; }
+public string ProductPicture { get; set; }
+public string ProductDescription { get; set; }
+public string ProductDescription_cn { get; set; }
+public string OriginalPrice { get; set; }
+public object LessPrice { get; set; }
+public string OfferPrice { get; set; }
+public object StartDate { get; set; }
+public object EndDate { get; set; }
+public object ProductReview { get; set; }
 public string PartitionKey { get; set; }
 public string RowKey { get; set; }
 public DateTime Timestamp { get; set; }
 public string ETag { get; set; }
 }
+
+
+
+
 
 
 
